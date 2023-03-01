932855e6640ea2186e34c19fc76f6fabc5295ebb
hiroHome <hirohiroherohero93@gmail.com>
2023-03-01 22:41:32 +0900

    Tuple to object 더 좋은 방법

--- 3.Tuple to Object/index.ts
+++ 3.Tuple to Object/index.ts
@@ -1,7 +1,7 @@
 {
     const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
 
-    type TupleToObject<T extends readonly any[]> = {
+    type TupleToObject<T extends readonly PropertyKey[]> = {
         [key in T[number]]: key;
     };
 
