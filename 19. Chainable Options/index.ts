b86f8239d982c2f640ef4a33fd67b6e3b82f8a75
hiroHome <hirohiroherohero93@gmail.com>
2023-05-28 12:42:02 +0900

    prettier

--- 19. Chainable Options/index.ts
+++ 19. Chainable Options/index.ts
@@ -2,32 +2,35 @@ declare const config: Chainable;
 
 // 내 시도
 type Chainable = {
-  option<K extends string, V>(key: K, value: V): Chainable & { [key in K]: V };
-  get(): ReturnType<Chainable["option"]>;
+    option<K extends string, V>(
+        key: K,
+        value: V
+    ): Chainable & { [key in K]: V };
+    get(): ReturnType<Chainable["option"]>;
 };
 
 const result = config
-  .option("foo", 123)
-  .option("name", "type-challenges")
-  .option("bar", { value: "Hello World" })
-  .get();
+    .option("foo", 123)
+    .option("name", "type-challenges")
+    .option("bar", { value: "Hello World" })
+    .get();
 
 // 결과는 다음과 같습니다:
 interface Result {
-  foo: number;
-  name: string;
-  bar: {
-    value: string;
-  };
+    foo: number;
+    name: string;
+    bar: {
+        value: string;
+    };
 }
 
 {
-  // 정답
-  type Chainable<T = {}> = {
-    option<K extends string, V>(
-      key: K,
-      value: V
-    ): Chainable<Omit<T, K> & Record<K, V>>;
-    get(): T;
-  };
+    // 정답
+    type Chainable<T = {}> = {
+        option<K extends string, V>(
+            key: K,
+            value: V
+        ): Chainable<Omit<T, K> & Record<K, V>>;
+        get(): T;
+    };
 }
